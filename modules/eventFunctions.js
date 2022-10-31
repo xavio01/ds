90e4dff62f5b8dea3b542b63d2ad77a6b416b8c0
xavio01 <116982849+xavio01@users.noreply.github.com>
2022-10-30 20:09:17 -0400

    Add files via upload

---
+++ modules/eventFunctions.js
@@ -0,0 +1,39 @@
+const bot = require('../bot.js').bot;
+const config = require('../config.json');
+const logger = require('./logger.js');
+const str = require('./stringificator.js');
+require('../bot.js');
+exports.consoleWarn = async (message, reason, count) => {
+	const { guild, channel, author } = message;
+
+	let entry = '';
+	const owner = await bot.users.fetch(channel.guild.ownerID);
+
+	const logAssets = {
+		server: `Server: ${str.stringifyGuild(guild)}, owner: ${str.stringifyUsers(owner)[0]}`,
+		channel: `Channel: ${str.stringifyChannel(channel)}`,
+		author: `Author: ${str.stringifyUsers(author)[0]}${message.webhookID ? ' - Webhook' : ''}`,
+		message: `Message content: ${message.embeds[0] !== undefined ? `${message.content}\n* Embed:\n${JSON.stringify(message.embeds[0], (key, value) => { if (value !== null) return value; }, 2)}` : message.content}`,
+	};
+
+	const addAsset = (...assets) => assets.forEach(asset => entry += `\n${logAssets[asset]}`);
+
+	if (reason === 'rateLimited') {
+		entry += `Channel is being rate limited! Count: ${count}, threshold: ${config.spamThreshold}`;
+		addAsset('server', 'channel', 'author', 'message');
+	}
+	else {
+		entry += reason;
+		addAsset('server', 'channel');
+	}
+
+	logger.log(entry, 'warn');
+};
+exports.publishConfirm = (message) => {
+	const { guild, channel, id } = message;
+	logger.log(`Published ${id} in ${str.stringifyChannel(channel)} - ${str.stringifyGuild(guild)}`);
+  
+ const SendChannel = bot.channels.cache.get('779629442204434473')
+ SendChannel.send(`Published ${id} in ${str.stringifyChannel(channel)} - ${str.stringifyGuild(guild)}`)
+ 
+};
